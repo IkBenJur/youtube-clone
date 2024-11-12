3a676328fb5ae2c1b244c0c61860dcab7f447d96
IkBenJur <jurspammer@gmail.com>
2024-11-12 22:47:33 +0100

    Locally process video

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,35 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath) {
+    res.status(400).send("Bad request: Missing input file path.");
+  }
+
+  if (!outputFilePath) {
+    res.status(400).send("Bad request: Missing output file path.");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") //convert video to 360p
+    .on("end", () => {
+      console.log("Processing finished successfully");
+      res.status(200).send("Processing finished successfully");
+    })
+    .on("error", (error) => {
+      console.log(`An error occured: ${error.message}`);
+      res.status(500).send(`Internal server error: ${error.message}`);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
